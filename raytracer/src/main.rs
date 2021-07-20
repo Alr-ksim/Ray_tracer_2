fdf12c0e32ce72f7bc318f06826fc95384ed00be
廖伟鑫 <liaoweixin@sjtu.edu.cn>
2021-07-20 15:40:08 +0800

    realsperlin

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -164,8 +164,8 @@ fn main() {
     const AS_RATIO: f64 = 16.0 / 9.0;
     const I_WID: i32 = 400;
     const I_HIT: i32 = (I_WID as f64 / AS_RATIO) as i32;
-    const SAMPLES: i32 = 10; //500
-    const MAXDEEP: i32 = 5; //50
+    const SAMPLES: i32 = 500; //500
+    const MAXDEEP: i32 = 50; //50
 
     let mut img: RgbImage = ImageBuffer::new(I_WID as u32, I_HIT as u32);
     let bar = ProgressBar::new(I_HIT as u64);
