b63a7ca5ff279b12bd49874bf10006ddfb892aa2
廖伟鑫 <liaoweixin@sjtu.edu.cn>
2021-07-20 15:54:45 +0800

    vec perlin

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -7,7 +7,7 @@ const PCNT: usize = 256;
 
 #[derive(Debug, Clone)]
 pub struct Perlin {
-    ranfloat: Vec<f64>,
+    ranvec: Vec<Vec3>,
     perm_x: Vec<usize>,
     perm_y: Vec<usize>,
     perm_z: Vec<usize>,
@@ -15,9 +15,9 @@ pub struct Perlin {
 
 impl Perlin {
     pub fn new() -> Self {
-        let mut rf: Vec<f64> = Vec::new();
+        let mut rv: Vec<Vec3> = Vec::new();
         for i in 0..PCNT {
-            rf.push(tools::randf(0.0, 1.0));
+            rv.push(Vec3::randvr(-1.0, 1.0));
         }
         let mut px: Vec<usize> = Vec::new();
         Perlin::perlin_generate_perm(&mut px);
@@ -26,7 +26,7 @@ impl Perlin {
         let mut pz: Vec<usize> = Vec::new();
         Perlin::perlin_generate_perm(&mut pz);
         Self {
-            ranfloat: rf,
+            ranvec: rv,
             perm_x: px,
             perm_y: py,
             perm_z: pz,
@@ -38,27 +38,23 @@ impl Perlin {
         let mut v = p.y() - p.y().floor();
         let mut w = p.z() - p.z().floor();
 
-        u = u * u * (3.0 - 2.0 * u);
-        v = v * v * (3.0 - 2.0 * v);
-        w = w * w * (3.0 - 2.0 * w);
-
         let i = p.x().floor() as i32;
         let j = p.y().floor() as i32;
         let k = p.z().floor() as i32;
 
-        let mut c: [[[f64; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
+        let mut c: [[[Vec3; 2]; 2]; 2] = [[[Vec3::zero(); 2]; 2]; 2];
 
         for di in 0..2 {
             for dj in 0..2 {
                 for dk in 0..2 {
-                    c[di][dj][dk] = self.ranfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                    c[di][dj][dk] = self.ranvec[self.perm_x[((i + di as i32) & 255) as usize]
                         ^ self.perm_y[((j + dj as i32) & 255) as usize]
                         ^ self.perm_z[((k + dk as i32) & 255) as usize]]
                 }
             }
         }
 
-        Perlin::trilinear_interp(&c, u, v, w)
+        Perlin::perlin_interp(&c, u, v, w)
     }
 
     fn perlin_generate_perm(v: &mut Vec<usize>) {
@@ -77,18 +73,23 @@ impl Perlin {
         }
     }
 
-    fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    fn perlin_interp(c: &[[[Vec3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let uu = u * u * (3.0 - 2.0 * u);
+        let vv = v * v * (3.0 - 2.0 * v);
+        let ww = w * w * (3.0 - 2.0 * w);
         let mut accum = 0.0;
+
         for i in 0..2 {
             for j in 0..2 {
                 for k in 0..2 {
                     let di = i as f64;
                     let dj = j as f64;
                     let dk = k as f64;
-                    accum += (di * u + (1.0 - di) * (1.0 - u))
-                        * (dj * v + (1.0 - dj) * (1.0 - v))
-                        * (dk * w + (1.0 - dk) * (1.0 - w))
-                        * c[i][j][k];
+                    let weight_v = Vec3::new(u - di, v - dj, w - dk);
+                    accum += (di * uu + (1.0 - di) * (1.0 - uu))
+                        * (dj * vv + (1.0 - dj) * (1.0 - vv))
+                        * (dk * ww + (1.0 - dk) * (1.0 - ww))
+                        * (c[i][j][k].clone() * weight_v);
                 }
             }
         }
